c28f4dd5652353461a001660f4c48b03843e9e6b
digital-nishtyak <lapaev.yarik@mail.ru>
2023-11-11 16:30:06 +0300

    The price is volatile now

--- src/main/java/edu/hw3/Task6/Stock.java
+++ src/main/java/edu/hw3/Task6/Stock.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class Stock {
     private static final Comparator<Stock> PRICE_COMPARATOR = Comparator.comparingInt(Stock::getPrice).reversed();
     private final String name;
-    private final int price;
+    private int price;
 
     public Stock(String name, int price) {
         this.name = name;
@@ -22,5 +22,9 @@ public class Stock {
 
     public String getName() {
         return name;
+    }
+
+    public void setPrice(int value) {
+        price = value;
     }
 }
